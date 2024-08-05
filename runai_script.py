a2ac9ed6e3c24ca8dcc0b304ea0b36d7c8a4d4e5
Shimon Malnick <shimonmalnick@gmail.com>
2024-08-05 09:08:11 +0300

    running on runai scripts

--- runai_script.py
+++ runai_script.py
@@ -6,11 +6,12 @@ import time
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('runs', type=int, default=1, help='Number of runs')
+    parser.add_argument('start', type=int, default=0, help='Start number')
     
     args = parser.parse_args()
     
     # Rest of your code goes here
-    for n in range(args.runs):
+    for n in range(args.start, args.start + args.runs):
         print("Submitting to runai number: ", n)
         command = f"runai submit --name shimon-attack-a-{n} -g 1.0 -i shimonmal/run_attacks:base --pvc=storage:/storage --large-shm -e HF_HOME=/storage/malnick/huggingface_cache -e OUTPUT_ROOT=/storage/malnick/concept_attacks/outputs/ -e MODELS_ROOT=/storage/malnick/concept_attacks/concept_models/ -e DATASETS_ROOT=/storage/malnick/concept_attacks/concept_datasets/ --command -- /bin/bash /storage/malnick/run_script.sh {n}"
         os.system(command)
--- running_pool.py
+++ running_pool.py
@@ -1,7 +1,7 @@
 import os
 import os.path as osp
 from src.types import ModelType
-
+import sys
 
 data_types_to_dir_names = {
     "Nudity": ["nudity"],
@@ -9,38 +9,68 @@ data_types_to_dir_names = {
     "VanGogh": ["vangogh"],
 }
 
-    
-datasets_root = os.environ['DATASETS_ROOT']
-models_root = os.environ['MODELS_ROOT']
-output_root = os.environ['OUTPUT_ROOT']
-model_bases = [
-        ("CompVis/stable-diffusion-v1-4", "sd_1_4"),
-        ("stabilityai/stable-diffusion-2-base", "sd_2"),
-        ("stabilityai/stable-diffusion-2-1-base", "sd_2_1"),
-    ]
 
-def get_next_job():
+datasets_root = os.environ["DATASETS_ROOT"]
+models_root = os.environ["MODELS_ROOT"]
+output_root = os.environ["OUTPUT_ROOT"]
+model_bases = [
+    ("CompVis/stable-diffusion-v1-4", "sd_1_4"),
+    # ("stabilityai/stable-diffusion-2-base", "sd_2"),
+    # ("stabilityai/stable-diffusion-2-1-base", "sd_2_1"),
+]
+
+
+def experiment_is_valid(dir_name, model_type, data_type):
+    data_name = "_".join([t.title() for t in osp.basename(dir_name).split("_")])
+    model_dir_name = osp.join(models_root, data_type.lower(), model_type)
+    return osp.exists(model_dir_name) and any(
+        [p for p in os.listdir(model_dir_name) if data_name in p]
+    )
+
+
+def get_next_job_params():
     for model_base, model_base_name in model_bases:
         for data_type in data_types_to_dir_names:
             for dir_name in data_types_to_dir_names[data_type]:
                 for model_type in ModelType.get_all_types_names():
-                    if not os.path.exists(f"{output_root}/{model_base_name}/{model_type}/{dir_name}"):
-                        return model_base, model_base_name, model_type, dir_name, data_type
-    return None
+                    if experiment_is_valid(
+                        dir_name, model_type, data_type
+                    ) and not osp.exists(
+                        f"{output_root}/{model_base_name}/{model_type}/{dir_name}"
+                    ):
+                        yield (
+                            model_base,
+                            model_base_name,
+                            model_type,
+                            dir_name,
+                            data_type,
+                        )
 
-def run_job(model_base, model_base_name, model_type, dir_name, data_type):
+
+def run_job(model_base, model_base_name, model_type, dir_name, data_type, dry_run=False):
     outdir = osp.join(output_root, model_base_name, model_type, dir_name)
     command = f"python run_attack_benchmark.py --model_base {model_base} --dataset_dir {osp.join(datasets_root, model_base_name, dir_name)} --model_type {model_type} --data_type {data_type} --out_dir {outdir}"
     print(f"Runnning command:\n{command}")
+    if dry_run:
+        return True
     if os.system(command) != 0:
         return False
 
+if __name__ == "__main__":
+    # all_params = list(get_next_job_params())
+    # if len(sys.argv) < 2:
+    #     for params in all_params:
+    #         run_job(*params, dry_run=False)
+    # else:
+    #     assert 0 <= int(sys.argv[1]) < len(all_params), f"Invalid index {sys.argv[1]}"
+    #     run_job(*all_params[int(sys.argv[1])], dry_run=False)
 
-while True:
-    next_job = get_next_job()
-    if next_job is None:
-        break
-    out = run_job(*next_job)
-    if not out:
-        break
-    
+    while True:
+        next_job_params_iterator = get_next_job_params()
+        try:
+            next_job_params = next(next_job_params_iterator)
+        except StopIteration:
+            break
+        out = run_job(*next_job_params)
+        if not out:
+            break
