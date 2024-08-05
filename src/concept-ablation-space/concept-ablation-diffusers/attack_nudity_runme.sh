1ac2d8dd34c1baeb23e310e19ba1b20da23c144e
Shimon Malnick <shimonmalnick@gmail.com>
2024-08-05 09:16:16 +0300

    moved files in directory concept-ablation-sapce

--- src/concept-ablation-space/concept-ablation-diffusers/attack_nudity_runme.sh
+++
@@ -1,5 +0,0 @@
-methods=('EraseDiff' 'ESD' 'FMN' 'Salun' 'Scissorhands' 'SPM' 'UCE')
-for method in "${methods[@]}"; do
-    echo "Inverting $method ...."
-    python concept-ablation-diffusers/attack_tamed_model_variable_nudity.py --method $method
-done
